5cdf02b0d55eba027fa0b01e94855931f573eaad
Brian Jones <beeceej.code@gmail.com>
2018-07-22 17:04:23 -0500

    spacing

--- guessing-game.lisp
+++ guessing-game.lisp
@@ -1,11 +1,11 @@
 (defun guessing-game (num-range tries target)
-    (format t "*Welcome to the guessing game, Would you like to play? (Y/N)*~%")
-    (let ((answer (read-line)))
-      (if (string-equal answer "Y")
-				(progn 
-				  (format t "*Guess a number 0 - ~a* ~%~%" num-range)
-					(is-winner? (.guessing-game 0 tries target) target))
-				(format t "*Cya*~%"))))
+	(format t "*Welcome to the guessing game, Would you like to play? (Y/N)*~%")
+	(let ((answer (read-line)))
+		(if (string-equal answer "Y")
+			(progn 
+				(format t "*Guess a number 0 - ~a* ~%~%" num-range)
+				(is-winner? (.guessing-game 0 tries target) target))
+			(format t "*Cya*~%"))))
 
 (defun .guessing-game (curr-try max-tries target)
 	(if (>= curr-try max-tries)
