48ba9e205d34dc2dd8aa17bb20a9b0030d7b24e8
Brian Jones <beeceej.code@gmail.com>
2018-07-22 17:03:14 -0500

    recursive refactor

--- guessing-game.lisp
+++ guessing-game.lisp
@@ -1,22 +1,20 @@
 (defun guessing-game (num-range tries target)
-  (let ((curr-try 0))
-    (format t "Welcome to the guessing game, Would you like to play? (Y / N)~%")
+    (format t "*Welcome to the guessing game, Would you like to play? (Y/N)*~%")
     (let ((answer (read-line)))
-        (if (string-equal answer "Y")
-            (progn (format t "Guess a number 0 - ~a ~%" num-range)
-                (loop
-                    (if (>= curr-try tries)
-                    (progn 
-                        (format t "Too many tries")
-                        (return)))
-                    (format t "This is guess ~a~%" curr-try)
-                    (let ((guess (read-line)))
-                        (if (eq (parse-integer guess) target)
-                            (progn
-                                (format t "You win!")
-                                (return))))
-                    (setf curr-try (+ curr-try 1))
-                ))
-            (format t "Bye ~%")))))
-  
-  
+      (if (string-equal answer "Y")
+				(progn 
+				  (format t "*Guess a number 0 - ~a* ~%~%" num-range)
+					(is-winner? (.guessing-game 0 tries target) target))
+				(format t "*Cya*~%"))))
+
+(defun .guessing-game (curr-try max-tries target)
+	(if (>= curr-try max-tries)
+	  ()
+		(progn (format t "*Guess ~a*~%" curr-try)
+		  (let ((guess (read-line)))
+				(if (eq (parse-integer guess) target)
+				  t
+					(.guessing-game (+ 1 curr-try) max-tries target))))))
+
+(defun is-winner? (b answer)
+  (if b (print "*You Win!*") (format t "~%*You Lose!*~%~%*The answer was ~a*" answer)))
